a33b6ebc2eddbace57579acab232820421d6a88b
Paugp2000 <a24paugonpal@inspedralbes.cat>
2026-05-26 15:19:11 +0200

    Creación de proyecto

---
+++ app/src/androidTest/java/com/example/gonzalezpau_piano_androidst/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.gonzalezpau_piano_androidst
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.gonzalezpau_piano_androidst", appContext.packageName)
+    }
+}
